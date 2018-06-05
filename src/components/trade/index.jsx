e42d56865727bba6d81cb166ee92aea3169aef5b
Mr.He <hexisen@tik.com>
2018-06-05 21:13:57 +0800

    更新trade页面

--- src/components/trade/index.jsx
+++ src/components/trade/index.jsx
@@ -1,31 +1,58 @@
 /*
  * @Author: Mr.He 
  * @Date: 2018-06-04 19:54:08 
- * @Last Modified by:   Mr.He 
- * @Last Modified time: 2018-06-04 19:54:08 
+ * @Last Modified by: Mr.He
+ * @Last Modified time: 2018-06-05 21:12:44
  * @content: 
  */
 
 import React, { Component } from "react";
-import { Button, Table } from "antd";
+import { Button, Table, Modal } from "antd";
 import * as uuid from "uuid";
+import reqwest from "reqwest";
+import moment from "moment-timezone";
 
 const columns = [{
     title: 'Name',
-    dataIndex: 'name',
-    sorter: true,
-    width: '20%',
+    dataIndex: 'alipay.realName',
 }, {
-    title: 'Gender',
-    dataIndex: 'gender',
-    filters: [
-        { text: 'Male', value: 'male' },
-        { text: 'Female', value: 'female' },
-    ],
-    width: '20%',
+    title: '支付宝账号',
+    dataIndex: 'alipay.account',
 }, {
-    title: 'Email',
-    dataIndex: 'email',
+    title: '总数',
+    dataIndex: 'totalSuply',
+}, {
+    title: '单价',
+    dataIndex: 'price',
+    render(text) {
+        return text + " ¥"
+    }
+}, {
+    title: '剩余',
+    dataIndex: 'restSuply',
+}, {
+    title: '创建日期',
+    dataIndex: 'createdAt',
+    render(text, record, index) {
+        return moment(text).tz("Asia/ShangHai").format("YYYY-MM-DD hh:mm:ss")
+    }
+}, {
+    title: '状态',
+    dataIndex: 'status',
+    render(text, record, index) {
+        // 1新建待审核 2正常 3下架 4非法
+
+        switch (text) {
+            case 1:
+                return "新建待审核";
+            case 2:
+                return "正常";
+            case 3:
+                return "下架";
+            default:
+                return "非法"
+        }
+    }
 }];
 
 export default class Trade extends Component {
@@ -33,22 +60,8 @@ export default class Trade extends Component {
         super();
 
         this.state = {
-            data: [{
-                id: "12133",
-                name: "Amy",
-                gender: "Man",
-                email: "45452@163.com"
-            }, {
-                id: "121331",
-                name: "Amy1",
-                gender: "Man",
-                email: "45452@163.com"
-            }, {
-                id: "1213223",
-                name: "Amy2",
-                gender: "Man",
-                email: "45452@163.com"
-            }],
+            data: [],
+            visible: false,
             pagination: {
                 pageSize: 20,
                 page: 15,
@@ -56,10 +69,55 @@ export default class Trade extends Component {
             },
             loading: false
         }
+
+        this.rowClick = this.rowClick.bind(this);
+        this.handleOk = this.handleOk.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
+    }
+    showModal() {
+        this.setState({
+            visible: true
+        })
+    }
+
+    handleOk() {
+        console.log("ok");
+        this.setState({
+            visible: false
+        })
+    }
+    handleCancel() {
+        console.log("cancel");
+        this.setState({
+            visible: false
+        })
     }
 
     change(pagination, filters, sorter) {
         console.log(pagination, filters, sorter);
+    }
+
+    rowClick(record) {
+        console.log(record, 123)
+        this.showModal();
+    }
+
+    componentDidMount() {
+        let _this = this;
+        reqwest({
+            url: "http://localhost:3003/v1/trade",
+            method: "get",
+            success(resp) {
+                console.log(11111, resp);
+
+                _this.setState({
+                    data: resp.data.rows
+                })
+            },
+            error(err) {
+                console.error(err);
+            }
+        });
     }
 
     render() {
@@ -68,25 +126,34 @@ export default class Trade extends Component {
                 <h1>
                     卖方挂单列表
                  </h1>
-                <div>
-                    <Button style={{ 'marginRight': '10px' }} size="large" type="primary">默认(未确认订单)</Button>
-                    <Button style={{ 'marginRight': '10px' }} size="large" type="primary">已确认订单</Button>
-                    <Button style={{ 'marginRight': '10px' }} size="large" type="primary">已卖完订单</Button>
+                <div className="mb10">
+                    <Button style={{ 'marginRight': '10px' }} size="large" type="default">默认(未确认订单)</Button>
+                    <Button style={{ 'marginRight': '10px' }} size="large" type="default">已确认订单</Button>
+                    <Button style={{ 'marginRight': '10px' }} size="large" type="default">已卖完订单</Button>
                 </div>
                 <Table columns={columns}
-                    rowKey={record => record.id}
+                    rowKey={record => record._id}
                     dataSource={this.state.data}
                     pagination={this.state.pagination}
                     loading={this.state.loading}
                     onChange={this.change}
                     onRow={(record) => {
                         return {
-                            onClick() {
-                                console.log(record, 123)
-                            }
+                            onClick: this.rowClick
                         }
                     }}
                 />
+
+                <Modal
+                    title="Basic Modal"
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                >
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Modal>
             </section>
 
         )
